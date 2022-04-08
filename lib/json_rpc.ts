5777476bc86f5fd788601a1204bb36b43957bce5
elhombre <elhombre.dev@gmail.com>
2022-04-08 23:57:55 +0500

    Make source code conform to eslint rules

--- lib/json_rpc.ts
+++ lib/json_rpc.ts
@@ -3,31 +3,31 @@
  */
 export const JsonRpcVersion = '2.0'
 
-export type JsonRpcRequestParams = Record<string, any> | any[]
+export type JsonRpcRequestParams = Record<string, unknown> | unknown[]
 
 export type JsonRpcId = 'auto' | number | string
 
 export interface IJsonRpcMessage {
-  readonly jsonrpc: string
+    readonly jsonrpc: string
 }
 
 export interface IJsonRpcNotification {
-  readonly method: string
-  readonly params?: JsonRpcRequestParams
+    readonly method: string
+    readonly params?: JsonRpcRequestParams
 }
 
 export interface IJsonRpcRequest extends IJsonRpcNotification {
-  readonly id: JsonRpcId
+    readonly id: JsonRpcId
 }
 
 export interface IJsonRpcError {
-  readonly code: number
-  readonly data?: any
-  readonly message: string
+    readonly code: number
+    readonly data?: unknown
+    readonly message: string
 }
 
 export interface IJsonRpcResponse {
-  readonly error?: IJsonRpcError
-  readonly id: JsonRpcId
-  readonly result?: any
+    readonly error?: IJsonRpcError
+    readonly id: JsonRpcId
+    readonly result?: unknown
 }
--- lib/rpc_response.ts
+++ lib/rpc_response.ts
@@ -1,55 +1,54 @@
-import {
-  IJsonRpcError,
-  IJsonRpcMessage,
-  IJsonRpcResponse,
-  JsonRpcId
-} from './json_rpc'
+import { IJsonRpcError, IJsonRpcMessage, IJsonRpcResponse, JsonRpcId } from './json_rpc'
 import { IRpcMessageArgs, RpcMessage } from './rpc_message'
+import { JsonObject } from 'type-fest'
 
 export interface IRpcResponse extends IJsonRpcMessage, IJsonRpcResponse {}
 
 export interface IRpcResponseArgs extends IRpcMessageArgs {
-  error?: IJsonRpcError
-  id: JsonRpcId
-  result?: any
+    error?: IJsonRpcError
+    id: JsonRpcId
+    result?: JsonObject
 }
 
 export class RpcResponse extends RpcMessage implements IRpcResponse {
-  readonly error?: IJsonRpcError
-  readonly id!: JsonRpcId
-  readonly result?: any
 
-  constructor(args: IRpcResponseArgs) {
-    super(args)
-    if (!!args.error === !!args.result) {
-      throw new Error(
-        'The RPC response must contain either an error or result data'
-      )
-    }
-    if (args.error) {
-      this.error = args.error
-    } else if (args.result) {
-      this.result = args.result
-    }
-  }
+    readonly error?: IJsonRpcError
 
-  toJSON() {
-    const { error, result } = this
-    return {
-      ...super.toJSON(),
-      ...(error ? { error } : {}),
-      ...(result ? { result } : {})
-    }
-  }
+    readonly id!: JsonRpcId
 
-  static makePropsFromJson(json: any): IRpcResponseArgs {
-    const props = super.makePropsFromJson(json) as IRpcResponseArgs
-    if (json.error) {
-      props.error = json.error
+    readonly result?: JsonObject
+
+    constructor (args: IRpcResponseArgs) {
+        super(args)
+        if (!!args.error === !!args.result) {
+            throw new Error('The RPC response must contain either an error or result data')
+        }
+        if (args.error) {
+            this.error = args.error
+        }
+        else if (args.result) {
+            this.result = args.result
+        }
     }
-    if (json.result) {
-      props.result = json.result
+
+    toJSON (): JsonObject {
+        const { error, result } = this
+        return {
+            ...super.toJSON(),
+            ...(error ? { error: (error as unknown) as JsonObject } : {}),
+            ...(result ? { result } : {})
+        }
     }
-    return props
-  }
+
+    static makePropsFromJson (json: JsonObject): IRpcResponseArgs {
+        const props = super.makePropsFromJson(json) as IRpcResponseArgs
+        if (json.error) {
+            props.error = (json.error as unknown) as IJsonRpcError
+        }
+        if (json.result) {
+            props.result = json.result as JsonObject
+        }
+        return props
+    }
+
 }
